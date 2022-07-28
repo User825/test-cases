f183de90af1f092ef8609061309a19c61a41e74b
Ichazov <ivan.chazov@gmail.com>
2022-07-28 09:56:58 +0300

    fix: header logo

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <header className="header">
-      <img width="40" src={`${process.env.PUBLIC_URL}logo512.png`} alt="logo" />
+      <img width="40" height="40" className="header__logo" src="https://robohash.org/aliquamcumqueiure.png" alt="logo" />
       <h1 className="header__title">Users list</h1>
     </header>
   )
