de71477d79c74ae40fb9599b90a0817ce2ff74d5
User825 <ivan.chazov@gmail.com>
2022-08-03 02:30:19 +0300

    Change games API options

--- src/api.js
+++ src/api.js
@@ -1,13 +1,19 @@
 import axios from "axios";
 
 export const fetchUsers = () => {
-  const endpoint = 'https://dummyjson.com/users?limit=100&select=firstName,lastName,email,image';
+  const url = 'https://dummyjson.com/users?limit=100&select=firstName,lastName,email,image';
   
-  return axios.get(endpoint).then(resp => resp.data.users);
+  return axios.get(url).then(resp => resp.data.users);
 }
 
 export const fetchGames = () => {
-  const endpoint = 'https://www.freetogame.com/api/games?platform=pc';
-
-  return axios.get(endpoint).then(resp => resp.data);
+  const options = {
+    method: 'GET',
+    url: 'https://free-to-play-games-database.p.rapidapi.com/api/games',
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+      'X-RapidAPI-Host': process.env.REACT_APP_RAPID_API_HOST
+    }
+  }
+  return axios.request(options).then(resp => resp.data);
 }
